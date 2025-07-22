39e029e2382f9a316c78f27e9d544897b5b54634
Palak_Soni <sonipalak2018@gmail.com>
2025-07-22 15:08:57 +0530

    Create print even numbers from 0 to 10.cpp

---
+++ print even numbers from 0 to 10.cpp
@@ -0,0 +1,26 @@
+//EXPERIMENT-6
+//Palak Soni
+//24070123069
+//ENTC A3
+
+#include <iostream>
+using namespace std;
+int main() {
+for(int i=0;i <=10 ;i++){
+    if(i%2==0){
+      cout<<i<<endl;  
+      i++;
+    }
+}    
+    return 0;
+}
+
+/*
+output:
+0
+2
+4
+6
+8
+10
+*/
