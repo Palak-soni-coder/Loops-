b5a8d07e2464393a326492254424e2dc30ffe20e
Palak_Soni <sonipalak2018@gmail.com>
2025-07-24 11:30:07 +0530

    Create print SIT.cpp

---
+++ print SIT.cpp
@@ -0,0 +1,23 @@
+// PALAK SONI
+// 24070123069
+// ENTC A3
+// EXPERIMENR-6
+// Problem Statement :Write a code to print "SIT" 5 times using FOR LOOP.
+
+
+#include <iostream>
+using namespace std;
+int main() {
+for(int i=0;i <5;i++){
+    cout<<"SIT"<<endl;
+}     
+      return 0;
+}
+
+/*Output-
+SIT
+SIT
+SIT
+SIT
+SIT
+*/
