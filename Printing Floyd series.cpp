f7f4329641a99f7c2d8b87bc0fb4598325dc364b
Palak_Soni <sonipalak2018@gmail.com>
2025-07-26 15:14:03 +0530

    Create Printing Floyd series.cpp

---
+++ Printing Floyd series.cpp
@@ -0,0 +1,31 @@
+//EXPERIMENT-6
+//PALAK SONI
+//24070123069
+//ENTC A3
+//TITLE: Print Floyd series.
+
+#include <iostream>
+using namespace std;
+int i,j;
+int n=4;
+int k=1;
+
+int main(){
+  for(i=1;i<=n;i++){
+      for(j=0;j<i;j++){
+          cout<< k << " ";
+          k++;
+      }
+      cout<<endl;
+  }
+  
+}
+
+
+/*Output:-
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+
+*/
